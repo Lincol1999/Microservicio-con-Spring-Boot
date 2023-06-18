3ee4fab0202074c140f13c28a49cf6c5aed9a1a2
Lincol1999 <lincol_mr_25@hotmail.com>
2023-06-17 23:14:15 -0500

    Eureka

--- eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
@@ -1,15 +1,16 @@
-package com.spring.microservice;
+package com.eureka.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class CarServiceApplication {
+@EnableEurekaServer
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
